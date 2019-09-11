f5e26e194117a46dc64b3e58e52c1d66761dc4bb
Ananda Zidane Satriani 12 <zidanesatriani2901@gmail.com>
2019-09-11 09:13:26 +0700

    unlinkFS

---
+++ minggu-7/tes/unlinkfs.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+fs.readFile('terserah.txt', async (err, data) => {
+    if(err) console.log("Data Tidak ditemukan!");
+    
+    await console.log("Data ditemukan"); 
+
+    fs.unlink('terserah.txt', (err) => {
+
+    if(err) return` console.log("Gagal hapus data");
+
+   console.log("Hapus data berhasil")
+
+    })
+})
