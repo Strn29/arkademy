05698481ae4ca8c1bdcd92e1236c89f9d9d09d13
Ananda Zidane Satriani 12 <zidanesatriani2901@gmail.com>
2019-12-10 14:42:23 +0700

    Ujian Praktek

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Background from './bgmoss.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Praktek extends Component {
+  render() {
+     return(
+      <div style={style.background}>             
+        <div style={style.text}>
+          <p style={{alignSelf: "left"}}>Go Green</p>
+        </div>
+          <div style={style.quotes}>
+            <h1 style={{alignSelf: "center", marginLet: "60"}}>Make it green</h1>
+              <span>Now more than ever. Our planet is need our</span>
+          </div>        
+                
+      </div>
+      );
+  }
 }
 
-export default App;
+let h = window.innerHeight;
+let w = window.innerWidth;
+const style = {
+    background: {
+        display: "flex",
+        backgroundImage: `url(${Background})`,
+        width: "100%",
+        height: "100%",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        justifyContent: "center",
+        alignItem: "center",
+        left: 0,
+        top: 0,
+        position: "absolute"
+    },
+    text: {
+        color: "white",
+        fontFamily: "Poppins",
+        top: 10,
+        left: 30,
+        border: 3, 
+        position: "absolute"
+    },
+    quotes: {
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItem: "center",
+        color: '#FFFFFF',
+        fontFamily: "Poppins",
+        textAlign: "center",
+        position: "absolute",
+        textAlign: "center",
+        padding: 200,
+        fontSize: 24,
+    },
+   
+}
+
+export default Praktek;
